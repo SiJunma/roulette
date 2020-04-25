2cec42bce382f27e51ef4d38bea5d2ecf0831ea1
Alina Onishchenko <30931781+SiJunma@users.noreply.github.com>
2020-04-25 23:42:43 +0300

    Add files via upload

--- js/task2.js
+++ js/task2.js
@@ -1,19 +1,71 @@
-const pattern = /^[\s]+$/;
-const half = 2;
-let wordInput = prompt('Please, input a word');
+const attempts = 3;
+let maxNumber = 5;
+let prizeValue = 0;
+let maxPrize = 100;
+let totalPrize = 0;
+let attempt = 1;
+// Because the line is too long
+const winMsg = `Congratulation, you won! Your prize is:`; 
 
-if (wordInput !== null && !pattern.test(wordInput) && wordInput) {
-    
-    if (wordInput.length%half) {
-        let wordIndex = Math.floor(wordInput.length/half);
-        alert(wordInput[wordIndex]);
-    } else {
-        let wordIndex1 = wordInput.length/half;
-        let wordIndex2 = wordInput.length/half-1;
+let doGame = confirm('Do you want to play a game?');
 
-        alert(wordInput[wordIndex2] + wordInput[wordIndex1]);
+while (doGame) {
+    // Generation before round starts
+    let prizeNumber = Math.floor(Math.random() * (maxNumber + 1));
+    prizeValue = maxPrize;
+    let userValue = +prompt(
+`Choose a roulette pocket number from 0 to ${maxNumber}
+Attempts left: ${attempts}
+Total prize: ${totalPrize}$
+Possible prize on current attempt: ${prizeValue}$`);
+
+    for (;;) {
+
+        if (userValue === prizeNumber) {
+            // Victory
+            totalPrize = totalPrize + prizeValue;
+            let doContinue = confirm(`${winMsg} ${prizeValue} $. Do you want to continue?`);
+
+            if (!doContinue) {
+                // Refusal to continue, Reset parameters
+                alert(`Thank you for your participation. Your prize is: ${totalPrize} $`);
+                doGame = confirm('Do you want to play again?');
+                attempt = 1;
+                maxPrize = 100;
+                maxNumber = 5;
+                totalPrize = 0;
+                break;
+            } else {
+                // Increase the parameters, keep playing.
+                maxPrize = maxPrize*2;
+                maxNumber= maxNumber+5;
+                attempt = 1;
+                break;
+            }
+
+        } else if (attempt < attempts) {
+            // Unsuccessful attempt
+            prizeValue = prizeValue/2;
+            userValue = +prompt(`Choose a roulette pocket number from 0 to ${maxNumber}
+Attempts left: ${attempts-attempt}
+Total prize: ${totalPrize}$
+Possible prize on current attempt: ${prizeValue}$`); 
+            attempt++
+        } else {
+            // Unsuccessful round, Reset parameters
+            alert(`Thank you for your participation. Your prize is: ${totalPrize} $`);
+            doGame = confirm('Do you want to play again?');
+            attempt = 1;
+            maxPrize = 100;
+            maxNumber = 5;
+            totalPrize = 0;
+            break;
+        }
     }
+} 
 
-} else {
-    alert('Invalid value');
+// Cancel the game
+if (!doGame) {
+    alert('You did not become a billionaire, but can.');
 }
+
